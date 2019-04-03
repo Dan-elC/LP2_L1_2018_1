dc817d852a8b889cac8f50ceab7807ca3032adbc
Alumno de Informática <alulab@imglabmp.lab.inf.pucp.edu.pe>
2019-04-03 16:01:35 -0500

    1.0

--- src/Chief.java
+++ src/Chief.java
@@ -1,4 +1,4 @@
-public class Chief{
+public class Chief extends Worker{
 	
 	private String passport;
 	private String ssn;
@@ -7,7 +7,30 @@ public class Chief{
 	public Chief(){
 
 	}
-	public Chief(String name, String surnameP, Sex sex, String dni, int age, StateC civilstate, double salary, TDedic typededication, int anexo){
-		
+	public Chief(String name, String surnameP, Sex sex, String dni, int age, StateC civilstate, double salary, TDedic typededication, int anexo, String passport, String ssn, String carnet){
+		super(name, surnameP, sex, dni, age, civilstate, salary, typededication, anexo);
+		this.passport=passport;
+		this.ssn=ssn;
+		this.carnet=carnet;
+	}
+
+	public String getPassport(){
+		return passport;
+	}
+	public void setPassport(String passport){
+		this.passport=passport;
+	}
+	public String getSsn(){
+		return ssn;
+	}
+	public void setSsn(String ssn){
+		this.ssn=ssn;
+	}
+	public String getCarnet(){
+		return carnet;
+	}
+	public void setCarnet(String carnet){
+		this.dni=carnet;
+		this.carnet=carnet;
 	}
 }
