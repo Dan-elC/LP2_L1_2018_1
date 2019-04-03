dae828144e9cca55ec31780c6c5de7b79de5f057
Alumno de Informática <alulab@imglabmp.lab.inf.pucp.edu.pe>
2019-04-03 15:39:22 -0500

    1.0

---
+++ src/Chief.java
@@ -0,0 +1,13 @@
+public class Chief{
+	
+	private String passport;
+	private String ssn;
+	private String carnet;
+
+	public Chief(){
+
+	}
+	public Chief(String name, String surnameP, Sex sex, String dni, int age, StateC civilstate, double salary, TDedic typededication, int anexo){
+		
+	}
+}
